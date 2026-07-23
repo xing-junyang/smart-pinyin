e7a688dc2c3af2dd97736d421bd5c199f7c33fa2
xingjunyang <xingjunyang@smail.nju.edu.cn>
2026-07-23 15:09:51 +0800

    Initial Commit

---
+++ smart pinyin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  smart pinyin
+//
+//  Created by xingjunyang on 2026/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ smart pinyin/smart_pinyinApp.swift
@@ -0,0 +1,17 @@
+//
+//  smart_pinyinApp.swift
+//  smart pinyin
+//
+//  Created by xingjunyang on 2026/7/23.
+//
+
+import SwiftUI
+
+@main
+struct smart_pinyinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
